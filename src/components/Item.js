fbe70370b7ba0d4b36be20c955f4c72f9375df7f
majazocom <maja.gedda@zocom.se>
2021-03-11 12:05:32 +0100

    link to specific shoe

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Item = (props) => {
+    const id = props.data.id;
     return (
         <section>
             <img></img>
             <h1>{props.data.brand}</h1>
             <p>I like this shoe</p>
-            <Link to='/'>Go to shoe</Link>
+            <Link to={ '/shop:' + id }>Go to shoe</Link>
         </section>
     );
 }
